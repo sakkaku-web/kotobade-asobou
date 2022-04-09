d33e0a4e6dc98cebb0688485c27974348481f79b
myin <minyin142p@gmail.com>
2022-04-09 14:37:01 +0200

    Package components for publish

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+// import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import del from 'rollup-plugin-delete';
+
+const packageJson = require('./package.json');
+export default {
+    input: 'src/game-components/index.ts',
+    output: [
+        {
+            file: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: packageJson.module,
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        del({ targets: 'dist/*' }),
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        typescript({
+            tsconfig: 'tsconfig.build.json',
+            useTsconfigDeclarationDir: true,
+        }),
+        postcss(),
+        terser(),
+    ],
+};
